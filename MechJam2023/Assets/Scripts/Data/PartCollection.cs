136c7457b237a53bcd5f0a9cba45359e8b398783
GreenOpal <antony.573@gmail.com>
2023-07-17 20:20:06 +0100

    Add function for getting mech part by type and/or element

--- MechJam2023/Assets/Scripts/Data/PartCollection.cs
+++ MechJam2023/Assets/Scripts/Data/PartCollection.cs
@@ -1,4 +1,5 @@
 
+using System.Linq;
 using UnityEngine;
 
 namespace MechJam {
@@ -10,6 +11,23 @@ namespace MechJam {
         public MechPart GetRandomPart()
         {
             return new MechPart(AllParts[Random.Range(0, AllParts.Length)]);
+        }
+        public MechPart GetPart(int ChosenTier = 0, bool ChooseElement = false, Element ChosenElement = default)
+        {
+            System.Collections.Generic.IEnumerable<MechPartData> validParts = AllParts;
+            if (ChosenTier > 0)
+            {
+                validParts = validParts.Where((part) => part.Tier == ChosenTier);
+            }
+            if (ChooseElement)
+            {
+                validParts = validParts.Where((part) => part.Element == ChosenElement);
+            }
+            var partsArray = validParts.ToArray();
+            var part = partsArray[Random.Range(0, partsArray.Length)];
+            Debug.LogWarning($"Get part {part}: tier {part.Tier}, element {part.Element}");
+
+            return new MechPart(partsArray[Random.Range(0, partsArray.Length)]);
         }
     }
 }
